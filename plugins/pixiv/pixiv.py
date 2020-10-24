9f07db0818e45d710fe50cec964f09052e8dd499
glaze <luyiping1011@gmail.com>
2020-10-24 16:18:32 -0700

    refactor as method

--- plugins/pixiv/pixiv.py
+++ plugins/pixiv/pixiv.py
@@ -1,16 +1,18 @@
-from pixivapi import Client
-from pathlib import Path
-from pixivapi import Size
-import json
 import datetime
-from datetime import date
+import json
 import os
+from datetime import date
+from pathlib import Path
+
+from pixivapi import Client, Size
 from pyexiv2 import Image
 
 # user bookmark is sort by date, newsest on the top
 # download all newsest like to assets/date folder
 
 # edit xmp data
+
+
 def xmp_edit(illust, img_path):
     img = Image(
         img_path, encoding='utf-8')
@@ -34,78 +36,87 @@ def xmp_edit(illust, img_path):
     img.modify_xmp(xmp_data)
     img.close()
 
-# read config
-account={}
-with open('config.json') as f:
-    account = json.load(f)
-    f.close()
 
-# get the last image id to continue
-old_id = ''
-if not os.path.exists('caches/pixiv_glazec_likes.json'):
-    with open('caches/pixiv_glazec_likes.json', 'w') as f:
-        f.close()
-else:
-    with open('caches/pixiv_glazec_likes.json', 'r') as f:
-        old_like = json.load(f)
-        old_id = old_like['illustrations'][0]['id']
+def pixiv():
+    # read config
+    account = {}
+    with open('config.json') as f:
+        account = json.load(f)
         f.close()
 
-# login
-client = Client(language='zh-CN')
-client.login(account['pixiv']['username'], account['pixiv']['password'])
-
-# get all new bookmark image info
-current_page = client.fetch_user_bookmarks(
-    account['pixiv']['id'], max_bookmark_id=None)
-likes = current_page['illustrations']
-# maximum number of image info to get
-n = 20
-k = 1
-while old_id not in [x.id for x in current_page['illustrations']] and k <= n//30:
-    next_page = client.fetch_user_bookmarks(
-        16139335, max_bookmark_id=current_page['next'])
-    likes = likes+next_page['illustrations']
-    current_page = next_page
-    k = k+1
-try:
-    likes = likes[:[x.id for x in current_page['illustrations']].index(old_id)]
-except ValueError:
-    pass
-
-print(f'Found {len(likes)} new images',end='\r')
-
-# Write new image info to like.json
-if len(likes) > 0:
-    with open('caches/pixiv_glazec_likes.json', 'w') as f:
-        json.dump({'illustrations': [{'id': x.id} for x in likes]}, f)
-
-# create current date folder. Correct the date for late night.
-current_date = '0'
-if datetime.datetime.now().hour < 3:
-    current_date = date.today()-datetime.timedelta(days=1)
-else:
-    current_date = date.today()
-if not os.path.exists(f'assets/{current_date}'):
-    os.makedirs(f'assets/{current_date}')
-
-# download image by id
-for illust in likes:
-    illust.download(
-        directory=Path(f'assets/{current_date}'),
-        size=Size.ORIGINAL,
-    )
-    if(illust.page_count == 1):
-        try:
-            xmp_edit(illust, f'assets/{current_date}/{str(illust.id)}.jpg')
-        except RuntimeError:
-            xmp_edit(illust, f'assets/{current_date}/{str(illust.id)}.png')
+    # get the last image id to continue
+    old_id = ''
+    if not os.path.exists('caches/pixiv_glazec_likes.json'):
+        with open('caches/pixiv_glazec_likes.json', 'w') as f:
+            f.close()
     else:
-        for p in range(illust.page_count):
+        with open('caches/pixiv_glazec_likes.json', 'r') as f:
+            old_like = json.load(f)
+            old_id = old_like['illustrations'][0]['id']
+            f.close()
+
+    # login
+    client = Client(language='zh-CN')
+    client.login(account['pixiv']['username'], account['pixiv']['password'])
+
+    print('Checking Pixiv new likes....')
+    # get all new bookmark image info
+    current_page = client.fetch_user_bookmarks(
+        account['pixiv']['id'], max_bookmark_id=None)
+    likes = current_page['illustrations']
+    # maximum number of image info to get
+    n = 20
+    k = 1
+    while old_id not in [x.id for x in current_page['illustrations']] and k <= n//30:
+        next_page = client.fetch_user_bookmarks(
+            16139335, max_bookmark_id=current_page['next'])
+        likes = likes+next_page['illustrations']
+        current_page = next_page
+        k = k+1
+    try:
+        likes = likes[:[
+            x.id for x in current_page['illustrations']].index(old_id)]
+    except ValueError:
+        pass
+
+    # Write new image info to like.json
+    if len(likes) > 0:
+        print(f'Found {len(likes)} new images')
+        with open('caches/pixiv_glazec_likes.json', 'w') as f:
+            json.dump({'illustrations': [{'id': x.id} for x in likes]}, f)
+    else:
+        print('Up to Date')
+
+    # create current date folder. Correct the date for late night.
+    current_date = '0'
+    if datetime.datetime.now().hour < 3:
+        current_date = date.today()-datetime.timedelta(days=1)
+    else:
+        current_date = date.today()
+    if not os.path.exists(f'assets/{current_date}'):
+        os.makedirs(f'assets/{current_date}')
+
+    # download image by id
+    for illust in likes:
+        illust.download(
+            directory=Path(f'assets/{current_date}'),
+            size=Size.ORIGINAL,
+        )
+        if(illust.page_count == 1):
             try:
-                xmp_edit(
-                    illust, f'assets/{current_date}/{str(illust.id)}/{str(illust.id)}_p{p}.jpg')
+                xmp_edit(illust, f'assets/{current_date}/{str(illust.id)}.jpg')
             except RuntimeError:
-                xmp_edit(
-                    illust, f'assets/{current_date}/{str(illust.id)}/{str(illust.id)}_p{p}.png')
-    print(f'Download {illust.id}')
+                xmp_edit(illust, f'assets/{current_date}/{str(illust.id)}.png')
+        else:
+            for p in range(illust.page_count):
+                try:
+                    xmp_edit(
+                        illust, f'assets/{current_date}/{str(illust.id)}/{str(illust.id)}_p{p}.jpg')
+                except RuntimeError:
+                    xmp_edit(
+                        illust, f'assets/{current_date}/{str(illust.id)}/{str(illust.id)}_p{p}.png')
+        print(f'Download {illust.id}')
+
+
+if __name__ == "__main__":
+    pixiv()
--- plugins/twitter/twitter.py
+++ plugins/twitter/twitter.py
@@ -10,8 +10,7 @@ import datetime
 from pyexiv2 import Image
 
 
-
-def xmp_edit(img_path,creator,description,title,dateCreated,source,label):
+def xmp_edit(img_path, creator, description, title, dateCreated, source, label):
     img = Image(
         img_path, encoding='utf-8')
     tags = []
@@ -32,65 +31,68 @@ def xmp_edit(img_path,creator,description,title,dateCreated,source,label):
     img.close()
 
 
-# read config and get creds
-with open('config.json') as f:
-    data =json.load(f)
-    creds = data['twitter']
-    f.close()
-
-# get the last post id to continue
-old_ids = []
-if not os.path.exists('caches/twitter_glazec_likes.ndjson'):
-    with open('caches/twitter_glazec_likes.ndjson', 'w') as f:
-        f.close()
-else:
-    with open('caches/twitter_glazec_likes.ndjson', 'r', encoding="utf_8") as f:
-        old_likes = ndjson.load(f)
-        old_ids = [i['id_str'] for i in old_likes]
+def twitter():
+    # read config and get creds
+    with open('config.json') as f:
+        data = json.load(f)
+        creds = data['twitter']
         f.close()
 
-# get likes info
-auth = tweepy.OAuthHandler(creds["consumer_key"], creds["consumer_secret"])
-auth.set_access_token(creds["access_token"], creds["access_token_secret"])
-api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-count = 0
-print('Checking new likes....')
-
-
-likes=[]
-for status in tweepy.Cursor(api.favorites, creds["username"],
-                            count=200, include_entities=True, tweet_mode='extended').items():
-    count = count + 1
-    status_id = str(status.id)
-    if status_id in old_ids:
-        break
+    # get the last post id to continue
+    old_ids = []
+    if not os.path.exists('caches/twitter_glazec_likes.ndjson'):
+        with open('caches/twitter_glazec_likes.ndjson', 'w') as f:
+            f.close()
     else:
-        likes.append(status._json)
-        print(f'Found {count} new likes',end='\r')
-        time.sleep(60 * 15 / (15 * 200))
+        with open('caches/twitter_glazec_likes.ndjson', 'r', encoding="utf_8") as f:
+            old_likes = ndjson.load(f)
+            old_ids = [i['id_str'] for i in old_likes]
+            f.close()
 
-# Write new likes post into likes.ndjson
-if len(likes)>0:
-    with open('caches/twitter_glazec_likes.ndjson', 'w') as f:
-        ndjson.dump(likes,f)
-        f.close()
+    # get likes info
+    auth = tweepy.OAuthHandler(creds["consumer_key"], creds["consumer_secret"])
+    auth.set_access_token(creds["access_token"], creds["access_token_secret"])
+    api = tweepy.API(auth, wait_on_rate_limit=True,
+                     wait_on_rate_limit_notify=True)
+    count = 0
+    print('Checking twitter new likes....')
 
-# create current date folder. Correct the date for late night.
-current_date = '0'
-if datetime.datetime.now().hour < 3:
-    current_date = date.today()-datetime.timedelta(days=1)
-else:
-    current_date = date.today()
-if not os.path.exists(f'assets/{current_date}'):
-    os.makedirs(f'assets/{current_date}')
+    likes = []
+    for status in tweepy.Cursor(api.favorites, creds["username"],
+                                count=200, include_entities=True, tweet_mode='extended').items():
+        count = count + 1
+        status_id = str(status.id)
+        if status_id in old_ids:
+            print('Up to Date')
+            break
+        else:
+            likes.append(status._json)
+            print(f'Found {count} new likes', end='\r')
+            time.sleep(60 * 15 / (15 * 200))
 
-# download images
-image_base_path='img/'
-pics = [fav for fav in likes if 'media' in fav['entities']]
-print(f'Found Total {len(pics)} likes containing medias')
+    # Write new likes post into likes.ndjson
+    if len(likes) > 0:
+        with open('caches/twitter_glazec_likes.ndjson', 'w') as f:
+            ndjson.dump(likes, f)
+            f.close()
 
-# notice gif file
-for fav in pics:
+    # create current date folder. Correct the date for late night.
+    current_date = '0'
+    if datetime.datetime.now().hour < 3:
+        current_date = date.today()-datetime.timedelta(days=1)
+    else:
+        current_date = date.today()
+    if not os.path.exists(f'assets/{current_date}'):
+        os.makedirs(f'assets/{current_date}')
+
+    # download images
+    image_base_path = 'img/'
+    pics = [fav for fav in likes if 'media' in fav['entities']]
+    if len(pics) > 0:
+        print(f'Found Total {len(pics)} likes containing medias')
+
+    # notice gif file
+    for fav in pics:
         print(fav['full_text'])
         for image in fav['extended_entities']['media']:
             img_data = requests.get(
@@ -100,3 +102,7 @@ for fav in pics:
             xmp_edit(os.path.join(f'assets/{current_date}', f"{image['id_str']}.jpg"), fav['user']['name'],
                      fav['full_text'].split('https://t.co/')[0], '', fav['created_at'], "https: // t.co/"+fav['full_text'].split('https://t.co/')[1], fav['entities']['hashtags'])
             time.sleep(60 * 15 / (15 * 200))
+
+
+if __name__ == "__main__":
+    twitter()
